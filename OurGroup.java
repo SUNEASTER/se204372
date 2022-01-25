cc41abc72ac7a5cff7a79f56218fc5fa11ccb480
ThanachoteI <91516544+ThanachoteI@users.noreply.github.com>
2022-01-25 14:34:46 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -2,16 +2,15 @@ import java.util.Vector;
 
 public class OurGroup {
 
-    private Vector<String> groupMembers;
-    public OurGroup()
-    {
-        groupMembers = new Vector<String>();
-        groupMembers.add("Kiattichai Pornvisawaluksakul"); //PUT YOUR NAME HERE
-	groupMembers.add("Thanachote Inchan");
-
-    }
-    public Vector<String> getGroupMembers () {
-        return groupMembers;
-    }
+	private Vector<String> groupMembers;
+	public OurGroup()
+	{
+		groupMembers = new Vector<String>();
+		groupMembers.add("Kiattichai Pornvisawaluksakul"); //PUT YOUR NAME HERE
+		groupMembers.add("Thanachote Inchan");
+	}
+	public Vector<String> getGroupMembers () {
+		return groupMembers;
+	}
 
 }
